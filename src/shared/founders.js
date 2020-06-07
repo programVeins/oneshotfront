c3310127455fdceb8ad21e3cb8973990e3f2620c
Sabesh Bharathi <sabesh5601@gmail.com>
2020-06-08 01:30:21 +0530

    PNGd images

--- src/shared/founders.js
+++ src/shared/founders.js
@@ -3,7 +3,7 @@ export const founders = [
         id: 0,
         name: "Darshan Nahar",
         role: "CEO & Founder of OneShot affiliate",
-        img: "/assets/images/darshan.PNG",
+        img: "/assets/images/darshan.png",
         description: "At the age of 19 Years, he had an Idea strike along with his entrepreneurial buddy Fazin for a startup which is exciting, entertaining and Mainly has a Earning motive for both the consumers and the company as well. He has always loved doing business be it small or large scale. He is also an event organizer and the owner of Backdoor events. With not much of investment in their pockets as this business was full of risks. The module of this program striked during the Lockdown period where in everyone were facing money shortage crisis. So then he came up with an idea of using affiliate marketing and salesmanship of people in a different way. He has been pursuing Law at Christ University, Bangalore. Right from a young age he was passionate about dance,gaming and other extra co curricular activities which he has tried to bring in through this website. Right from an early age of five Darshan has been doing advertisements as a Child Artist and has done 100 plus advertisements . Such firm belief on Anything is possible brings him to this web based startup where in its a win win situation for one and all.",
     },
     {
@@ -17,7 +17,7 @@ export const founders = [
         id: 2,
         name: "Sabesh Bharathi",
         role: "Web Developer",
-        img: "/assets/images/sabesh.png",
+        img: "/assets/images/sabesh.PNG",
         description: "Greetings! I am Sabesh, and I developed the website your peering at right now. I'm the lead technician for OneShot Affiliate. I'm persuing my engineering (IT) from SSN College of Engineering, Chennai. When not petting dogs or eating maggi, you can find me coding away obvliously for hours, with only the computer screen and the coffee mug in my focus. I've always wanted to create (Develop and Design) robust products that users can handle easily, with as much functionality as possible. OneShot Affiliate is my debut project, and I'm glad I teamed up to make this possible. Feel free to hit my instagram @sabeshbharathi for anything from intense coding discussions to talking about Rick and Morty. Cheers!"
     },
 ]
--- src/shared/professionals.js
+++ src/shared/professionals.js
@@ -3,49 +3,49 @@ export const professionals =  [
         id: 0,
         name: "Sumukh",
         course: "Legal Sciences",
-        img: "/assets/images/professionals/1.jpeg",
+        img: "/assets/images/professionals/1.png",
         description: "Sumukh who is currently pursuing BBA LLB at Christ University Bangalore, and has been awarded the following: BEST MOOTER - 3rd National Anand College of Legal Studies Moot and Honourable Commendation - Harvard Model United Nations 2018"
     },
     {
         id: 1,
         name: "Satvik",
         course: "Economics",
-        img: "/assets/images/professionals/2.jpg",
+        img: "/assets/images/professionals/2.png",
         description: "Satvik is a Financial analyst and has worked with IDBI Capital, Diligent Financial Services and virtual Internship with Citi & BCG. He has been the President of Toastmasters'. Youth Leadership Program  and he himself is a leadership coach & teacher who is a National Level sports player and has participated in more than 25 fests accross Bangalore."
     },
     {
         id: 2,
         name: "Veeresh",
         course: "Youtube Course",
-        img: "/assets/images/professionals/3.jpg",
+        img: "/assets/images/professionals/3.png",
         description: "Veeresh is a college student and a school topper. He is a YouTuber also known as HolyFrenchFry. He excels in Public relations and has won events in college for the same. Also participating in various extracurricular activities like Football and Tennis and activities like Mime and Theatre for acting related. He has completed courses and is certified in Sales, Digital Marketing and Negotiation online. He recently surpassed a thousand Subscribers and fourty thousand views on YouTube."
     },
     {
         id: 3,
         name: "Brian",
         course: "Home Fitness",
-        img: "/assets/images/professionals/4.jpg",
+        img: "/assets/images/professionals/4.png",
         description: "Hi I'm Brian D'Cruz. I am an athlete, boxer and soccer player. My main events being long jump,100 &200m. I got into boxing when I was 11 years old and have been winning gold medals throughout my school and high school days. I was awarded the most sporting boxer award in 12th grade and the best boxer in 8th grade. My real passion is for soccer and  I'm currently playing for a local team called the Eridians in Pune"
     },
     {
         id: 4,
         name: "Ehsaas",
         course: "Zumba Dance",
-        img: "/assets/images/professionals/5.jpg",
+        img: "/assets/images/professionals/5.png",
         description: "EHSAAS - 'a feeling', that's what dance means to us \u2764 Now let's know a little bit about the founders of Ehsaas. Vishal, the one who's loved for his style and is known as a powerhouse because of the charismatic and energetic vibe that he creates when he's around. Nirali, the one who is appreciated for her elegant moves and graceful expressions, has started dancing since childhood. She's a bundle of joy and creativity. Past 2 years we've choreographed many sangeets and organized fun and creative events for our clients. According to us, Zumba is the best way to lose calories as you can do it while losing yourself to the beautiful rythm of music. So we are here with a set of fun zumba routines for 'everybody and every body'. Do follow us on Instagram @ehsaas_bynv and subscribe to our youtube channel (Ehsaas- Nirali & Vishal) for amazing dance routines."
     },
     {
         id: 5,
         name: "Akshay Taleda",
         course: "Life Transformation",
-        img: "/assets/images/professionals/6.jpg",
+        img: "/assets/images/professionals/6.png",
         description: "Akshay has worked with several millennials and Gen-X helping them discover & Unleash their potential. He believes that an individual's life is the result of their interpretation of their experiences. Akshay's mission in life to transform the lives of millions and touch the lives of a billion people through his seminars&writing. His life's ultimate mission is to build systems that simplify lives. He looks to transform the current generation of people by destroying their limiting beliefs which will lead us onto better incoming future generations. He aims to change the narrative about intelligence and build an emotionally intelligent society that will transform the way world functions."
     },
     {
         id: 6,
         name: "The Adventurous Palate",
         course: "Cooking",
-        img: "/assets/images/professionals/7.jpg",
+        img: "/assets/images/professionals/7.png",
         description: "Akshay has worked with several millennials and Gen-X helping them discover & Unleash their potential. He believes that an individual's life is the result of their interpretation of their experiences. Akshay's mission in life to transform the lives of millions and touch the lives of a billion people through his seminars&writing. His life's ultimate mission is to build systems that simplify lives. He looks to transform the current generation of people by destroying their limiting beliefs which will lead us onto better incoming future generations. He aims to change the narrative about intelligence and build an emotionally intelligent society that will transform the way world functions."
     }
 
